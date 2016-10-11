35d20f23807617393f3fe3605d9e3b33bcb61441
Alumne <Alumne@192.168.22.33>
2016-10-11 13:33:18 +0200

    Això es un canvi fet a les 13:33

--- JavaApplication6/src/javaapplication6/JavaApplication6.java
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -15,7 +15,7 @@ public class JavaApplication6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello two");
+        System.out.println("Hello three");
     }
     
 }
